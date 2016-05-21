1ef274ef48eb1922ea12e7371876beb1a4a68de5
Matthew Brown <me.matt.brown@gmail.com>
2016-05-21 13:07:04 -0400

    Version script added

---
+++ xcode-version-script.swift
@@ -0,0 +1,81 @@
+#!/usr/bin/env xcrun --sdk macosx swift
+
+/*
+ Copyright (c) 2016 Matthew Brown
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+import Foundation
+import Darwin
+
+// Utils
+
+func shell(args: String ...) -> (statusCode: Int, output: String) {
+    let task = NSTask()
+    task.launchPath = "/usr/bin/env"
+    task.arguments = args
+
+    let pipe = NSPipe()
+    task.standardOutput = pipe
+    task.launch()
+    task.waitUntilExit()
+
+    let data = pipe.fileHandleForReading.readDataToEndOfFile()
+    return (Int(task.terminationStatus), String(data: data, encoding: NSUTF8StringEncoding)!.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet()))
+}
+
+func setPlistProperty(property: String, toValue value: Any) {
+    let environment = NSProcessInfo.processInfo().environment
+
+    guard let projectDir = environment["PROJECT_DIR"] else {
+        print("\'PROJECT_DIR\' environment variable not set!")
+        exit(1)
+    }
+    guard let infoPlistFile = environment["INFOPLIST_FILE"] else {
+        print("\'INFOPLIST_FILE\' environment variable not set!")
+        exit(1)
+    }
+
+    if shell("/usr/libexec/PlistBuddy", "-c", "Set :\(property) \(value)", "\(projectDir)/\(infoPlistFile)").statusCode == 0 {
+        print("Set \(property) to \(value)")
+    } else {
+        print("Failed to set \(property) to \(value)")
+        exit(1)
+    }
+}
+
+// Script
+
+shell("sh", "/etc/profile")
+let gitExec = shell("which", "git").output
+let branchName = shell(gitExec, "rev-parse", "--abbrev-ref", "HEAD").output
+
+guard branchName.hasPrefix("release") else {
+    print("Not in release branch, not updating version numbers.")
+    exit(0)
+}
+
+guard let versionStartIndex = branchName.rangeOfString("/")?.endIndex else {
+    print("Release branch should have form \"release/x.y.z\".")
+    exit(1)
+}
+
+let versionNumberString = branchName.substringFromIndex(versionStartIndex)
+let versionNumberPattern = "^\\d+(\\.\\d+)*$"
+guard (try! NSRegularExpression(pattern: versionNumberPattern, options: [])).numberOfMatchesInString(versionNumberString, options: [], range: NSRange(location: 0, length: versionNumberString.characters.count)) > 0 else {
+    print("Version number should match regex pattern \"\(versionNumberPattern)\".")
+    exit(1)
+}
+
+let rcNumber = Int(shell("git", "rev-list", "dev..", "--count").output)! + 1
+
+let shortVersionString = "\(versionNumberString)-rc\(rcNumber)"
+let buildNumber = shell(gitExec, "rev-list", "--all", "--count").output
+
+setPlistProperty("CFBundleShortVersionString", toValue: shortVersionString)
+setPlistProperty("CFBundleVersion", toValue: buildNumber)
